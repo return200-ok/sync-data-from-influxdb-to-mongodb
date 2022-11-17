69b83133c0b63eb2dc2464113c2684ecb385ac8c
caolv <caolv@biplus.com.vn>
2022-11-17 16:09:07 +0700

    .

--- migrate_influx_mongo.py
+++ migrate_influx_mongo.py
@@ -22,37 +22,6 @@ query = 'from(bucket: "gitlab") |> range(start: -7d)'
 influx_client = InfluxClient(influx_server, influx_token, org_name, bucket_name)
 mongo_client = MongoDB(collection, database, url, port)
 
-data_test = [{
-          "result": "_result",
-          "table": 24,
-          "_start": "2022-11-10T08:22:47.137716+00:00",
-          "_stop": "2022-11-17T08:22:47.137716+00:00",
-          "_time": "2022-11-15T07:45:50+00:00",
-          "_value": "c1d900b7",
-          "_field": "commit_id",
-          "_measurement": "4",
-          "author_email": "admin@example.com",
-          "commit_id": "c1d900b706be2c560f959ae5ab84285d1b84efc7",
-          "commit_title": "Add new file",
-          "created_at": "2022-11-09T07:50:17.000Z",
-          "project_name": "d"
-     },
-     {
-          "result": "_result",
-          "table": 25,
-          "_start": "2022-11-10T08:22:47.137716+00:00",
-          "_stop": "2022-11-17T08:22:47.137716+00:00",
-          "_time": "2022-11-11T02:56:09+00:00",
-          "_value": "c1d900b7",
-          "_field": "short_id",
-          "_measurement": "4",
-          "author_email": "admin@example.com",
-          "commit_id": "c1d900b706be2c560f959ae5ab84285d1b84efc7",
-          "commit_title": "Add new file",
-          "created_at": "2022-11-09T07:50:17.000Z",
-          "project_name": "d"
-     }
-]
 data_point = influx_client.query_response_to_json(query)
 # print(type(data_point))
 for mongo_document in data_point:
